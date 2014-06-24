29b2308b979f749a310a9701915654ca922c0518
Hsu Benson <bensonhsu2013@gmail.com>
2014-06-24 21:21:06 +0800

    Initial Commit & skeleton files

---
+++ extract-files.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BASE=../../../vendor/samsung/lt02wifi/proprietary
+rm -rf $BASE/*
+
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  # My way of pulling blobs without the device
+  cp ~/dev/lt02wifi_sysdump/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
